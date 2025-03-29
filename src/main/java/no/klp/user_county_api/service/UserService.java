cd92f4293908b86ac5566798440d43db2bd84c3f
LMIsaksen <lisemari.isaksen@gmail.com>
2025-03-29 23:17:11 +0100

    Add UserService with createUser method

---
+++ src/main/java/no/klp/user_county_api/service/UserService.java
@@ -0,0 +1,21 @@
+package no.klp.user_county_api.service;
+
+
+import no.klp.user_county_api.model.User;
+import no.klp.user_county_api.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
+
