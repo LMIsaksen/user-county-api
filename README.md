b55f8e3ec22fbbdd6224efca78c2bc4b192cd5f3
LMIsaksen <lisemari.isaksen@gmail.com>
2025-03-29 22:51:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+A Spring Boot project with REST endpoints to:
+•	Create and retrieve users in an in-memory H2 database
+•	Look up county names via the Kartverket API based on county number
+
+Run the project with the following command:
+```mvn spring-boot:run```
---
+++ src/main/java/no/klp/user_county_api/UserCountyApiApplication.java
@@ -0,0 +1,13 @@
+package no.klp.user_county_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCountyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCountyApiApplication.class, args);
+	}
+
+}
