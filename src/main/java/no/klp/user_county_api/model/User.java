e277c73f0a0f32e9844a9988206c63e31aed18e2
LMIsaksen <lisemari.isaksen@gmail.com>
2025-03-29 23:01:01 +0100

    Add User entity

---
+++ src/main/java/no/klp/user_county_api/model/User.java
@@ -0,0 +1,52 @@
+package no.klp.user_county_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String type;
+
+    public User() {
+    }
+
+    public User(Long id, String email, String type) {
+        this.id = id;
+        this.email = email;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
