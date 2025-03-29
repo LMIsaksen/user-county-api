2ba7cd9f9c7d2140290aee0950226e3eaa048446
LMIsaksen <lisemari.isaksen@gmail.com>
2025-03-29 23:25:54 +0100

    Implement GET endpoint retrieve user by Id

--- src/main/java/no/klp/user_county_api/controller/UserController.java
+++ src/main/java/no/klp/user_county_api/controller/UserController.java
@@ -4,10 +4,7 @@ import jakarta.validation.Valid;
 import no.klp.user_county_api.model.User;
 import no.klp.user_county_api.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -23,5 +20,14 @@ public class UserController {
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User created = userService.createUser(user);
         return ResponseEntity.ok(created);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/no/klp/user_county_api/service/UserService.java
+++ src/main/java/no/klp/user_county_api/service/UserService.java
@@ -17,5 +17,9 @@ public class UserService {
     public User createUser(User user) {
         return userRepository.save(user);
     }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
 }
 
